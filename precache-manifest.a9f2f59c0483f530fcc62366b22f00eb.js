7b83daea6dc524f8afc92f32517daa1df60ae03c
streloc84 <streloc84@gmail.com>
2020-01-31 17:35:34 +0400

    Updates

---
+++ precache-manifest.a9f2f59c0483f530fcc62366b22f00eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30f5d90f766acf887df21f8df1281a8",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "e7027c5e9cd5995ebe56",
+    "url": "/myapp/static/css/main.dd0a6252.chunk.css"
+  },
+  {
+    "revision": "e71ba9dbd4a5c9d75bbe",
+    "url": "/myapp/static/js/2.92e4bd99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.92e4bd99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7027c5e9cd5995ebe56",
+    "url": "/myapp/static/js/main.7c0a7a72.chunk.js"
+  },
+  {
+    "revision": "9df814497166441517a5",
+    "url": "/myapp/static/js/runtime-main.e311d370.js"
+  },
+  {
+    "revision": "f38166e0bcbb7663f0d2834ffd52addf",
+    "url": "/myapp/static/media/b52_[allfont.ru].f38166e0.ttf"
+  },
+  {
+    "revision": "05d39315961d90930e1ab50e5f964d35",
+    "url": "/myapp/static/media/paper_clip.05d39315.svg"
+  },
+  {
+    "revision": "3c0f6fd6628902b2a8f2a3021b8862d5",
+    "url": "/myapp/static/media/textured_paper.3c0f6fd6.png"
+  }
+]);
